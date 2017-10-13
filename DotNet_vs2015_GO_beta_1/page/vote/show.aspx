5ae1e11b16003433bdc17c0c81d14edd6d2fb92f
czj <392067117@qq.com>
2017-10-13 21:23:49 +0800

    [System_Initialize_2017_08_26]    czj 392067117@qq.com
    1.添加global中的VoteInte函数
    2.简单实现投票页面

---
+++ DotNet_vs2015_GO_beta_1/page/vote/show.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="show.aspx.cs" Inherits="DotNet_vs2015_GO_beta_1.page.vote.show" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title> 赛事GO 投票评分结果 </title>
+    <script>
+             function myrefresh()
+             {
+                 window.location.href="show.aspx";
+                   
+             }
+ 
+             setTimeout(myrefresh,2000); //指定2秒刷新一次
+              
+     </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height:100px;position:relative; padding:10px;">
+        <div id="bar1" runat ="server" style="width:20px;position:absolute; bottom:0px;margin-bottom:20px;display:inline;background-color:blue;margin-left:5%;"></div>
+        <div id="bar2" runat ="server" style="width:20px;position:absolute; bottom:0px;margin-bottom:20px;display:inline;background-color:blue;margin-left:15%;"></div>
+        <div id="bar3" runat ="server" style="width:20px;position:absolute; bottom:0px;margin-bottom:20px;display:inline;background-color:blue;margin-left:25%;"></div>
+        <div id="bar4" runat ="server" style="width:20px;position:absolute; bottom:0px;margin-bottom:20px;display:inline;background-color:blue;margin-left:35%;"></div>
+        <div id="bar5" runat ="server" style="width:20px;position:absolute; bottom:0px;margin-bottom:20px;display:inline;background-color:blue;margin-left:45%;"></div>
+        <div id="bar6" runat ="server" style="width:20px;position:absolute; bottom:0px;margin-bottom:20px;display:inline;background-color:blue;margin-left:55%;"></div>
+        <div id="bar7" runat ="server" style="width:20px;position:absolute; bottom:0px;margin-bottom:20px;display:inline;background-color:blue;margin-left:65%;"></div>
+        <div id="bar8" runat ="server" style="width:20px;position:absolute; bottom:0px;margin-bottom:20px;display:inline;background-color:blue;margin-left:75%;"></div>
+    </div>
+    <div> 
+        <asp:Label ID="Label1" runat="server" Text="Label" style="display:inline"></asp:Label>
+        <asp:Label ID="Label2" runat="server" Text="Label" style="display:inline"></asp:Label>
+        <asp:Label ID="Label3" runat="server" Text="Label" style="display:inline"></asp:Label>
+        <asp:Label ID="Label4" runat="server" Text="Label" style="display:inline"></asp:Label>
+        <asp:Label ID="Label5" runat="server" Text="Label" style="display:inline"></asp:Label>
+        <asp:Label ID="Label6" runat="server" Text="Label" style="display:inline"></asp:Label>
+        <asp:Label ID="Label7" runat="server" Text="Label" style="display:inline"></asp:Label>
+        <asp:Label ID="Label8" runat="server" Text="Label" style="display:inline"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ DotNet_vs2015_GO_beta_1/page/vote/show.aspx.designer.cs
@@ -0,0 +1,168 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能导致不正确的行为，如果
+//     重新生成代码，则所做更改将丢失。
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace DotNet_vs2015_GO_beta_1.page.vote {
+    
+    
+    public partial class show {
+        
+        /// <summary>
+        /// form1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// bar1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bar1;
+        
+        /// <summary>
+        /// bar2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bar2;
+        
+        /// <summary>
+        /// bar3 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bar3;
+        
+        /// <summary>
+        /// bar4 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bar4;
+        
+        /// <summary>
+        /// bar5 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bar5;
+        
+        /// <summary>
+        /// bar6 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bar6;
+        
+        /// <summary>
+        /// bar7 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bar7;
+        
+        /// <summary>
+        /// bar8 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl bar8;
+        
+        /// <summary>
+        /// Label1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Label2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Label3 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Label4 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Label5 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Label6 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// Label7 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+        
+        /// <summary>
+        /// Label8 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+    }
+}
